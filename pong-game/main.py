9d19b1d6afe927dd58ee29e295e71fa5ab142a28
Senan Warnock <senanw95@gmail.com>
2022-09-10 17:23:22 +0100

    finish pong game

--- pong-game/main.py
+++ pong-game/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 
@@ -13,7 +14,7 @@ screen.tracer(0)
 r_paddle = Paddle((350 , 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
-
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -23,14 +24,32 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_on = True
 while game_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
     #detect collisions with the walls
     if ball.ycor() > 280 or ball.ycor() < -280:
         #needs to bounce
-        ball.bounce()
+        ball.bounce_y()
+    
+    #detect collisions with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+        sleep_time -= 0.0001
 
+
+    #if r_paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+        sleep_time = 0.1
+    
+    #if l_paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+        sleep_time = 0.1
+    
 
 screen.exitonclick()
